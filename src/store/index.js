267f765c50a73c35e9e36172a77e116eb50743a1
yunabc <450387582@qq.com>
2017-02-09 16:53:00 +0800

    all done

--- src/store/index.js
+++ src/store/index.js
@@ -2,22 +2,20 @@
 
 import Vue from 'vue'
 import Vuex from 'vuex'
+import mutations from './mutations'
+import actions from './actions'
 
 Vue.use(Vuex)
 
 // 先写个假数据
 
 const state = {
-	totalTime:0,
-	list:[{
-		name:'二货',
-		avatar:'https://sfault-avatar.b0.upaiyun.com/147/223/147223148-573297d0913c5_huge256',
-		date:'2016-12-12',
-		totalTime:'6',
-		comment:'12月25日晚上，陪女朋友一起过圣诞节需要6个小时'
-	}]
+	totalTime: 0,
+  list: []
 }
 
 export default new Vuex.Store({
-	state
+	state,
+	mutations,
+  actions
 })
